00667e387bde22ea53f96c33bd14a6ed34cef002
BrunoAccorsi <b.bergoli@gmail.com>
2023-12-15 12:57:09 -0700

    added about

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-// Smooth Scroll
+window.onload = function() {
+  showDivFromHash();
+};
+
+window.onhashchange = function() {
+  showDivFromHash();
+};
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -9,3 +17,25 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+
+function showDivFromHash() {
+  var hash = window.location.hash.substr(1); // Get the hash from the URL
+  var divs = document.querySelectorAll('section[id]');
+
+  if (hash == '') {
+    var home = document.getElementById('home');
+    home.classList.remove('hide');
+    home.classList.add('show');
+    return;
+  }
+  
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i].id === hash) {
+      divs[i].classList.remove('hide');
+      divs[i].classList.add('show');
+    } else {
+      divs[i].classList.remove('show');
+      divs[i].classList.add('hide');
+    }
+  }
+}
